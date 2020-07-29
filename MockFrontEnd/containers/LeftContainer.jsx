73a79be3501d2cbdfaff5717be22bbb8b49af638
JIMvsTHEWORLD <JMArmbruster@gmail.com>
2020-07-29 12:23:43 -0400

    adding create customer form

--- MockFrontEnd/containers/LeftContainer.jsx
+++ MockFrontEnd/containers/LeftContainer.jsx
@@ -7,6 +7,22 @@ class LeftContainer extends React.Component {
         return(
             <div className='LeftContainer'>
                 Hello LeftContainer
+                <form className='CreateCustomerForm'>
+                    <h4>Create New Customer</h4>
+                    <label>Customer ID:  </label>
+                    <input id="custId" type="text"></input>
+                    <br />
+                    <label>Customer Name:  </label>
+                    <input id="custName" type="text"></input>
+                    <br />
+                    <label>Customer Age:  </label>
+                    <input id="custAge" type="text"></input>
+                    <br />
+                    <label>Customer Address:  </label>
+                    <input id="custAddress" type="text"></input>
+                    <br />
+                    <button type="button" id="createCustomerButton">Create Customer</button>
+                </form>
             </div>
         )
     }
