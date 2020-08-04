44d0904c133928ba55a975d3b12a87eeeac738a6
JIMvsTHEWORLD <JMArmbruster@gmail.com>
2020-08-04 10:21:55 -0400

    commit before merging

--- HorusDatabaseModel.js
+++ HorusDatabaseModel.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const { Schema } = mongoose;
 
 // DB link for horus tracer data.
-const horus_db_uri = `${process.env.HORUS_DB}`;
-// const horus_db_uri = 'mongodb+srv://tinyturtle2:horuspass@cluster0-4egmg.mongodb.net/horus?retryWrites=true&w=majority';
+// const horus_db_uri = `${process.env.HORUS_DB}`;
+const horus_db_uri = 'mongodb+srv://tinyturtle2:horuspass@cluster0-4egmg.mongodb.net/horus?retryWrites=true&w=majority';
 
 mongoose
   .connect(horus_db_uri, { useNewUrlParser: true, useUnifiedTopology: true })
