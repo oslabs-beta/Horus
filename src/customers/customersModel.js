904097fc44b6f6c173b0af55a821059742438c29
Daria <46409352+bondarinka@users.noreply.github.com>
2020-08-07 16:24:12 -0400

    Update customersModel.js

--- src/customers/customersModel.js
+++ src/customers/customersModel.js
@@ -5,9 +5,6 @@ const { Schema } = mongoose;
 
 // DB link for customers data.
 const customers_db_uri = `${process.env.CUSTOMERS_DB}`;
-// const customers_db_uri = 'mongodb+srv://tinyturtle2:horuspass@cluster0-4egmg.mongodb.net/customers?retryWrites=true&w=majority'
-
-// const URI = process.env.MONGO_URI || myURI;
 
 mongoose.connect(customers_db_uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected!!!********* Customer Database is live!!!'))
